262daf4215bf709383126a8804061e22188f1c15
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-04-29 19:46:59 -0400

    able to display a list of known tasks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Task } from './interfaces/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'GC-Lab-Angular';
+  tasks:Task[] = [
+    {text:"Do the Dishes", completed:false},
+    {text:"Take Dog for walke", completed:true},
+    {text:"GC Lab 16.3 Task list", completed:false},
+    {text:"Eat Dinner", completed:true},
+  ];
+  toggleTask = (task:Task) => {
+    task.completed = !task.completed;
+  } 
 }
