94420446c36806864e2c55c4f024e47c6add9443
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-04-29 21:38:15 -0400

    able to add and remove tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,25 @@ import { Task } from './interfaces/task';
 
 export class AppComponent {
   title = 'GC-Lab-Angular';
-  tasks:Task[] = [
-    {text:"Do the Dishes", completed:false},
-    {text:"Take Dog for walke", completed:true},
-    {text:"GC Lab 16.3 Task list", completed:false},
-    {text:"Eat Dinner", completed:true},
+  tasks: Task[] = [
+    { text: "Do the Dishes", completed: false },
+    { text: "Take Dog for walke", completed: true },
+    { text: "GC Lab 16.3 Task list", completed: false },
+    { text: "Eat Dinner", completed: true },
   ];
-  toggleTask = (task:Task) => {
+  toggleTask = (task: Task) => {
     task.completed = !task.completed;
-  } 
+  };
+  newTask: string = "";
+  addTask = () => {
+    if (this.newTask.length > 0) {
+      this.tasks.push({ text: this.newTask, completed: false });
+      this.newTask = "";
+    }
+  }
+
+  removeTask = (task: Task) => {
+    this.tasks = this.tasks.filter( t => t != task);
+  }
+
 }
